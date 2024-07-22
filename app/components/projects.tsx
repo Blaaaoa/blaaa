57fd5a070344429949ace812238f4cac7d05fe9e
soman73 <notaugusto7@gmail.com>
2024-07-23 01:24:01 +0530

    Update projects.tsx

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,36 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-const projects = () => {
+const Projects = () => {
   return (
-    <div className='p-12'>
-       <div className=" mt-24 shadow-lg rounded-lg border-2 shadow-gray-400 font-mono  ">
-      <div className="flex flex-wrap justify-around mt-3  mb-3">
-        <div className='flex flex-col items-center'>
-          <div className=' text-3xl'>100</div>
-          <div className='text-md'>project</div>
-        </div>
-
-
-        <div className='flex flex-col items-center'>
-          <div className=' text-3xl'>1M+</div>
-          <div className='text-md'>Users</div>
-        </div>
-        <div className='flex flex-col items-center'>
-          <div className=' text-3xl'>10</div>
-          <div className='text-md'>Awards</div>
-        </div>
-        <div className='flex flex-col items-center'>
-          <div className=' text-3xl'>5</div>
-          <div className='text-md'>years</div>
+    <div className='px-5 lg:px-20 py-10'>
+      <div className='mt-24 shadow-lg rounded-lg border-2 shadow-gray-400 font-mono'>
+        <div className='flex flex-wrap justify-around py-6'>
+          <div className='flex flex-col items-center p-4'>
+            <div className='text-3xl'>100</div>
+            <div className='text-md'>Projects</div>
+          </div>
+          <div className='flex flex-col items-center p-4'>
+            <div className='text-3xl'>1M+</div>
+            <div className='text-md'>Users</div>
+          </div>
+          <div className='flex flex-col items-center p-4'>
+            <div className='text-3xl'>10</div>
+            <div className='text-md'>Awards</div>
+          </div>
+          <div className='flex flex-col items-center p-4'>
+            <div className='text-3xl'>5</div>
+            <div className='text-md'>Years</div>
+          </div>
         </div>
       </div>
     </div>
-
-
-
-    </div>
-   
-  )
+  );
 }
 
-export default projects
+export default Projects;
